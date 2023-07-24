be4fc7fb2698d5992819dfcbff11769500bc2f4b
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-07-24 09:09:05 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -23,3 +23,14 @@ export const TransactionHistory = ({transactions}) => {
     </tbody>
     </Table>
 )};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+      PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          type: PropTypes.string.isRequired,
+          amount: PropTypes.string.isRequired,
+          currency: PropTypes.string.isRequired,
+      }).isRequired
+  ).isRequired,
+}
